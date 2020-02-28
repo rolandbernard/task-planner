38bd3b97ffd9aed53ec208248306a1e8d82bb0ff
Roland Bernard <rolli.bernard@gmail.com>
2020-02-28 23:56:16 +0100

    Added emscripten bindings

---
+++ optimizer/src/optimizer.cpp
@@ -0,0 +1,45 @@
+
+#include <emscripten.h>
+#include <emscripten/bind.h>
+
+#include <vector>
+
+#include "task-planner.h"
+
+EMSCRIPTEN_BINDINGS(Bindings) {
+    emscripten::value_object<PlannedTask>("PlannedTask")
+        .field("worker", &PlannedTask::worker)
+        .field("client", &PlannedTask::client)
+        .field("day", &PlannedTask::day)
+        .field("time_of_day", &PlannedTask::time_of_day);
+    emscripten::value_object<Worker>("Worker")
+        .field("name", &Worker::name)
+        .field("address", &Worker::address)
+        .field("maximum_time", &Worker::maximum_time)
+        .field("lon", &Worker::lon)
+        .field("lat", &Worker::lat);
+    emscripten::value_object<Client>("Client")
+        .field("name", &Client::name)
+        .field("address", &Client::address)
+        .field("working_time", &Client::working_time)
+        .field("priority", &Client::priority)
+        .field("lon", &Client::lon)
+        .field("lat", &Client::lat);
+    emscripten::register_vector<PlannedTask>("VectorOfPlannedTask");
+    emscripten::register_vector<std::vector<PlannedTask>>("BectorOfVectorOfPlannedTask");
+    emscripten::register_vector<std::vector<std::vector<PlannedTask>>>("VectorOfVectorOfVectorOfPlannedTask");
+    emscripten::register_vector<Worker>("VectorOfWorker");
+    emscripten::register_vector<Client>("VectorOfClient");
+    emscripten::register_vector<long>("VectorOfLong");
+    emscripten::register_vector<std::vector<long>>("VectorOfVectorOfLong");
+    emscripten::class_<TaskPlanner>("TaskPlanner")
+        .constructor<>()
+        .function("setWorkers", &TaskPlanner::setWorkers)
+        .function("setClients", &TaskPlanner::setClients)
+        .function("setDurations", &TaskPlanner::setDurations)
+        .function("initialize", &TaskPlanner::initialize)
+        .function("getPlan", &TaskPlanner::getPlan)
+        .function("getPlanPerWorkerPerDay", &TaskPlanner::getPlanPerWorkerPerDay)
+        .function("optimize", &TaskPlanner::optimize);
+}
+
