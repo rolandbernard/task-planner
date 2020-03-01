35cc7d7bbba53f597e5e486310b9a21ea91ba567
Roland Bernard <rolli.bernard@gmail.com>
2020-03-01 19:40:56 +0100

    Now avoiding fetch on empty nominatim request

--- app/src/nominatim.js
+++ app/src/nominatim.js
@@ -2,20 +2,24 @@
 import config from './config';
 
 export async function getCoordinatedForAddress(address) {
-    try {
-        const result = await fetch(config.nominatimApiLocation + `/search?q=${address}&format=json&limit=1`);
-        if(result.status !== 200) {
-            throw new Error(`Code ${result.status}`);
-        } else {
-            const json = await result.json();
-            if(json.length === 0) {
-                throw new Error("Not found");
+    if(address) {
+        try {
+            const result = await fetch(config.nominatimApiLocation + `/search?q=${address}&format=json&limit=1`);
+            if(result.status !== 200) {
+                throw new Error(`Code ${result.status}`);
             } else {
-                return [parseFloat(json[0].lon), parseFloat(json[0].lat)];
+                const json = await result.json();
+                if(json.length === 0) {
+                    throw new Error("Not found");
+                } else {
+                    return [parseFloat(json[0].lon), parseFloat(json[0].lat)];
+                }
             }
+        } catch(e) {
+            throw e;
         }
-    } catch(e) {
-        throw e;
+    } else {
+        throw new Error("Not found");
     }
 }
 
