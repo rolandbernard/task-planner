eb0d842bb2b2a4f33f172605dd637f510b1ba912
Roland Bernard <rolli.bernard@gmail.com>
2020-04-29 11:20:58 +0200

    Changed OSRM server to demo server

--- app/src/config.js
+++ app/src/config.js
@@ -5,7 +5,7 @@ const config = {
         attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     },
     nominatim_api_location:   'https://nominatim.openstreetmap.org',
-    osrm_api_location:        'http://localhost:5000',
+    osrm_api_location:        'http://router.project-osrm.org/',
 }
 
 module.exports = config;
