cb8ef56c3f7845d389ead6d46156c84593995c26
Roland Bernard <rolli.bernard@gmail.com>
2020-05-11 21:49:53 +0200

    Added a cache to the nominatim api calls

--- app/src/nominatim.js
+++ app/src/nominatim.js
@@ -1,23 +1,35 @@
 
 import config from './config';
 
+let cache = { };
+
+if(typeof localStorage !== "undefined" && localStorage.getItem('nominatim_cache')) {
+    cache = JSON.parse(localStorage.getItem('nominatim_cache'));
+}
+
 export async function getCoordinatedForAddress(address) {
     if(address) {
-        try {
-            const result = await fetch(config.nominatim_api_location + `/search?q=${address}&format=json&limit=1`);
-            if(!result.ok) {
-                throw new Error(`Code ${result.status}`);
-            } else {
-                const json = await result.json();
-                if(json.length === 0) {
-                    throw new Error("Not found");
+        if(!cache[address]) {
+            try {
+                const result = await fetch(config.nominatim_api_location + `/search?q=${address}&format=json&limit=1`);
+                if(!result.ok) {
+                    throw new Error(`Code ${result.status}`);
                 } else {
-                    return [parseFloat(json[0].lon), parseFloat(json[0].lat)];
+                    const json = await result.json();
+                    if(json.length === 0) {
+                        throw new Error("Not found");
+                    } else {
+                        cache[address] = [parseFloat(json[0].lon), parseFloat(json[0].lat)];
+                        if(typeof localStorage !== "undefined") {
+                            localStorage.setItem('nominatim_cache', JSON.stringify(cache));
+                        }
+                    }
                 }
+            } catch(e) {
+                throw e;
             }
-        } catch(e) {
-            throw e;
         }
+        return cache[address];
     } else {
         throw new Error("Not found");
     }
